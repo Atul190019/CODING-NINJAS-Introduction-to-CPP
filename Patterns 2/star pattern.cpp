bf5dcca607a2ffa69766310337b18439286e45a7
Atul190019 <106909363+Atul190019@users.noreply.github.com>
2022-10-18 16:33:21 +0530

    Create star pattern.cpp

---
+++ Patterns 2/star pattern.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n-i)
+        {
+            cout<<" ";
+            j=j+1;
+        }
+        int k=1;
+        while(k<=(2*i-1))
+        {
+            cout<<"*";
+            k=k+1;
+        }
+        cout<<endl;
+        i=i+1;
+        
+    }
+  
+}
+
+
