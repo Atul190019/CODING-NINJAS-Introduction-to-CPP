374fd065b6ce13fcbde2f577626c48421f6a7b9b
Atul190019 <106909363+Atul190019@users.noreply.github.com>
2022-10-18 16:22:52 +0530

    Create square root.cpp

---
+++ operators and for loop/square root.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int n;
+    cin>>n;
+    int sq = 0;
+    
+    sq = sqrt(n);
+    cout<<sq;
+}
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+    int n;
+    cin>>n;
+    if(n==0)
+    {
+        cout<<0;
+    }
+    else if(n>=1 && n<4)
+    {
+        cout<<1;
+    }
+    else if(n>=4 && n<9)
+    {
+        cout<<2;
+    }
+    else if(n>=9 && n<16)
+    {
+        cout<<3;
+    }
+    else if(n>=16 && n<25)
+    {
+        cout<<4;
+    }
+    else if(n>=25 && n<36)
+    {
+        cout<<5;
+    }
+    else if(n>=36 && n<49)
+    {
+        cout<<6;
+    }
+    else if(n>=49 && n<64)
+    {
+        cout<<7;
+    }
+    else if(n>=64 && n<81)
+    {
+        cout<<8;
+    }
+    else if(n>=81 && n<100)
+    {
+        cout<<9;
+    }
+    else if(n>=100 && n<121)
+    {
+        cout<<10;
+    }
+    
+    
+    
+    
+	
+}*/
