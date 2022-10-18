9073b553a093167c4a509e8c5bbde5a3a67ea855
Atul190019 <106909363+Atul190019@users.noreply.github.com>
2022-10-18 16:38:57 +0530

    Create Interesting Alphabetss .cpp

---
+++ Patterns 1/Interesting Alphabetss .cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+  int N;
+    cin>>N;
+    
+    int i=1;
+    while(i<=N){
+        int j=1;
+        char ch='A'+N-i;
+        while(j<=i){
+            cout << ch;
+            ch=ch+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
